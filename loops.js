78ae212f8a8c1b4cbbff57ffafe0dff42f815285
Helen <nikishina.olena@gmail.com>
2018-01-13 02:26:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,7 +8,7 @@ function forLoop(array){
   return Math.random() >= 0;
  }
  
- function whieLoop (doWhileLoop(array)){
-   console.log(`"I am ${i} strange loops${i=== 0 ? '' : 's'}"`);
+ function whieLoop(n){
+   return console.log(`"I am ${i} strange loops${i=== 0 ? '' : 's'}"`);
  }
  
